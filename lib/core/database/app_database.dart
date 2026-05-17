4a367f7ec11ea184768f5daeac48b65bfc5d7289
Gabriel Herrera Waunous <pablogabrielherrera@gmail.com>
2026-05-17 13:33:26 -0300

    feat: define Drift tables and AppDatabase shell

---
+++ lib/core/database/app_database.dart
@@ -0,0 +1,26 @@
+// lib/core/database/app_database.dart
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'tables.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [Routines, WorkoutSessions, Exercises, Sets])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+  AppDatabase.forTesting(QueryExecutor e) : super(e);
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dir.path, 'workout.db'));
+    return NativeDatabase(file);
+  });
+}
---
+++ lib/core/database/tables.dart
@@ -0,0 +1,33 @@
+// lib/core/database/tables.dart
+import 'package:drift/drift.dart';
+
+class Routines extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text()();
+  TextColumn get rawText => text()();
+  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
+}
+
+class WorkoutSessions extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  DateTimeColumn get date => dateTime()();
+  TextColumn get notes => text().nullable()();
+}
+
+class Exercises extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get sessionId => integer().references(WorkoutSessions, #id)();
+  TextColumn get name => text()();
+  IntColumn get orderIndex => integer()();
+}
+
+@DataClassName('WorkoutSet')
+class Sets extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get exerciseId => integer().references(Exercises, #id)();
+  IntColumn get setNumber => integer()();
+  IntColumn get reps => integer().nullable()();
+  RealColumn get weight => real().nullable()();
+  BoolColumn get toFailure => boolean().withDefault(const Constant(false))();
+  BoolColumn get isPartial => boolean().withDefault(const Constant(false))();
+}
